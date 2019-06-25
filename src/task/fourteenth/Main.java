607530fcca0fbfb53a762422f19c854511de5e8e
Maryana <renvema@bigmir.net>
2019-06-25 09:52:02 +0300

    less14 commit3

--- src/task/fourteenth/Main.java
+++ src/task/fourteenth/Main.java
@@ -1,21 +1,19 @@
 package task.fourteenth;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Please enter text:");
-        Stream<String> stream = Arrays.stream(new Scanner(System.in, "UTF-8")
-                .nextLine()
-                .toLowerCase()
-                .split("[^0-9a-zA-Zа-яА-Я]+"))
+        new ArrayList<String>(Arrays.asList(new Scanner(System.in).nextLine().toLowerCase().split("[^а-яa-z0-9]+")))
                 .stream()
-                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.<String, Long>comparingByKey())
